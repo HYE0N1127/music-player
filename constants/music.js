4fe92b8c54a6c70a148164c7d7cab093b8ec7395
현빈 <hhb041127@naver.com>
2025-12-09 01:37:54 +0900

    add: initial

---
+++ constants/music.js
@@ -0,0 +1,26 @@
+export const musics = [
+  {
+    id: 1,
+    name: "돌아오지마 (Feat. 용준형 of 비스트)",
+    artist: "Heize",
+    source: "asset/music/dont-come-back.mp3",
+  },
+  {
+    id: 2,
+    name: "I",
+    artist: "TAEYEON",
+    source: "asset/music/i.mp3",
+  },
+  {
+    id: 3,
+    name: "EASY (Feat. Sik-K(식케이))",
+    artist: "휘인(마마무)",
+    source: "asset/music/easy.mp3",
+  },
+  {
+    id: 4,
+    name: "먹구름",
+    artist: "윤하 (YOUNHA)",
+    source: "asset/music/dark-cloud.mp3",
+  },
+];
