569def30655dc6413be828eb33a9c94b76af9dbc
현빈 <hhb041127@naver.com>
2025-12-09 22:13:46 +0900

    feat: business logic

---
+++ store/music-store.js
@@ -0,0 +1,72 @@
+import { MusicRepository } from "../repository/music-repository.js";
+import { State } from "../util/state.js";
+
+export class MusicStore {
+  #repository;
+  #state = new State({
+    currentMusic: undefined,
+    playlist: [],
+  });
+
+  constructor() {
+    this.#repository = new MusicRepository();
+  }
+
+  addToPlaylist(music) {
+    const state = this.#state.value;
+
+    const update = [...state.playlist, music];
+
+    this.#state.value = {
+      ...state,
+      playlist: update,
+    };
+
+    this.#repository.setPlaylist(state.playlist);
+  }
+
+  removeFromPlaylist(id) {
+    const state = this.#state.value;
+    const prev = this.#state.value.playlist;
+    const update = prev.filter((music) => music.id !== id);
+
+    this.#state.value = {
+      ...state,
+      playlist: update,
+    };
+
+    this.#repository.setPlaylist(state.playlist);
+  }
+
+  clearPlaylist() {
+    this.#state.value = {
+      currentMusic: undefined,
+      playlist: [],
+    };
+
+    this.#repository.clearPlaylist();
+    this.#repository.clearCurrentMusic();
+  }
+
+  isPlaying(id) {
+    return id === currentMusic.id;
+  }
+
+  fetch() {
+    const currentMusic = this.#repository.getCurrentMusic();
+    const currentPlaylist = this.#repository.getPlaylist();
+
+    this.#state.value = {
+      currentMusic: currentMusic,
+      playlist: currentPlaylist,
+    };
+  }
+
+  getPlaylist() {
+    return this.#state.value.playlist;
+  }
+
+  getCurrentMusic() {
+    return this.#state.value.currentMusic;
+  }
+}
